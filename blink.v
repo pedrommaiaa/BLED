12888e37e61d4b71b6db7fd430702e0aa45ab5db
Pedro Maia <pedrommaiaa@protonmail.com>
2022-07-08 08:33:35 -0400

    wip working

--- blink.v
+++ blink.v
@@ -1,13 +1,15 @@
-module blink 
-(
-    input clk,          // posedge clock
-    input clr,          // clear
-    output reg[3:0] cnt // counter 
-);
+module blink(input clk, output led);
+    reg	[23:0] cnt = 0;
+    reg temp_led = 1'b0;
+
     always @(posedge clk) begin
-        if (!clr)
+        cnt <= cnt + 1;
+        if (cnt >= 2500000) begin
+            temp_led <= !temp_led;
             cnt <= 0;
-        else
-            cnt <= cnt + 1;
+        end
     end
+
+    assign led = temp_led;
+
 endmodule
--- test_blink.v
+++ test_blink.v
@@ -1,33 +1,28 @@
-`timescale 1ns/100ps // 1 ns time unit, 100 ps resolution
+`timescale 1ns/1ns // Time unit and time precision
 
 module test_blink;
+    // Variables we will inspect
     reg clk;
-    always #5 clk = ~clk;
-    
-    reg clr;
-    wire [3:0] cnt;
-    
+    wire led;
 
-    blink blink_0
-    (
-        .clk(clk),
-        .clr(clr),
-        .cnt(cnt)
-    );
+    // Instantiate the blink module (uut = unit under test)
+    blink uut(clk, led);
+    
+    // Initial blocks run only once at the start of simulation
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(1, test_blink);
+
+        // Generate the 25MHz clock signal
+        forever begin
+            clk = 1'b0;
+            #40 // Delay for one cycle of 25MHz
+            clk = ~clk; // Flip the signal level
+        end
+    end
 
     initial begin
-        $dumpfile("wave.vcd");      // create a VCD waveform dump called "wave.vcd"
-        $dumpvars(0, test_blink);   // dump variable changes in the testbench
-
-        $monitor("t=%-4d: cnt = %d", $time, cnt);
-        
-        clk <= 0;
-        
-        #20 clr <= 1;
-        #40 clr <= 0;
-        #60 clr <= 1;
-        #80 clr <= 0;
-
-        #20 $finish();
+        #1000000000 // Delay for 1 second (10^9 because time unit is 1ns)
+        $finish; // Stop simulation (yeah, this stops the forever block as well)
     end
 endmodule
